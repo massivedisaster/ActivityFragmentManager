2b0ae4c89e54cdc4028b7e8150b08bb8d382efbe
Jorge Costa <j.costa90@gmail.com>
2017-06-07 20:26:57 +0100

    Added shared elements

--- sample/src/main/java/com/massivedisaster/example/feature/sharedelements/FragmentSharedElement.java
+++ sample/src/main/java/com/massivedisaster/example/feature/sharedelements/FragmentSharedElement.java
@@ -1,23 +1,34 @@
 package com.massivedisaster.example.feature.sharedelements;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 
 import com.massivedisaster.example.activitymanager.R;
 
-/**
- * SHared Element Fragment
- * Fragment to show the transaction
- */
 public class FragmentSharedElement extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_shared_element, container, false);
+        final View view = inflater.inflate(R.layout.fragment_shared_element, container, false);
+
+        view.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                view.getViewTreeObserver().removeOnPreDrawListener(this);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    getActivity().startPostponedEnterTransition();
+                }
+                return true;
+            }
+        });
+
+        return view;
     }
 }
