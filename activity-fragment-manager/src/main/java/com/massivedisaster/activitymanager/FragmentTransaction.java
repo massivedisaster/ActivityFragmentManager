c1deb7e7b81c58bb67906e14e63c55afbe3ce1f2
Jorge Costa <j.costa90@gmail.com>
2017-06-07 14:17:48 +0100

    Added documentation and refactoring class names

---
+++ activity-fragment-manager/src/main/java/com/massivedisaster/activitymanager/FragmentTransaction.java
@@ -0,0 +1,77 @@
+package com.massivedisaster.activitymanager;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+
+import com.massivedisaster.activitymanager.activity.AbstractFragmentActivity;
+import com.massivedisaster.activitymanager.animation.TransactionAnimation;
+
+/**
+ * Abstract transaction
+ */
+public abstract class FragmentTransaction implements ITransaction<FragmentTransaction> {
+
+    protected AbstractFragmentActivity mActivity;
+    protected Fragment mFragment;
+    protected android.support.v4.app.FragmentTransaction mFrgTransaction;
+    protected String mTag;
+
+    /**
+     * FragmentTransaction constructor, created to be used by an activity.
+     *
+     * @param activity      The activity to be used to add the new fragment.
+     * @param fragmentClass The Fragment to be injected in the activityClass.
+     */
+    @SuppressLint("CommitTransaction")
+    FragmentTransaction(AbstractFragmentActivity activity, Class<? extends Fragment> fragmentClass) {
+        mActivity = activity;
+
+        mFrgTransaction = mActivity.getSupportFragmentManager().beginTransaction();
+
+        try {
+            mFragment = fragmentClass.newInstance();
+            mFrgTransaction.addToBackStack(null);
+        } catch (InstantiationException e) {
+            Log.e(ActivityFragmentManager.class.getCanonicalName(), e.toString());
+        } catch (IllegalAccessException e) {
+            Log.e(ActivityFragmentManager.class.getCanonicalName(), e.toString());
+        }
+    }
+
+    @Override
+    public FragmentTransaction addTag(String tag) {
+        mTag = tag;
+        return this;
+    }
+
+    @Override
+    public FragmentTransaction addBundle(Bundle bundle) {
+        if (bundle != null) {
+            mFragment.setArguments(bundle);
+        }
+        return this;
+    }
+
+    /**
+     * Set the transaction animation to be passed to the new Fragment.
+     *
+     * @param transactionAnimation The animation to used in fragment transaction.
+     * @return Return the Transaction instance.
+     */
+    public FragmentTransaction addTransactionAnimation(TransactionAnimation transactionAnimation) {
+        // Apply the default activity animation if the FragmentTransaction is null.
+        if (transactionAnimation == null) {
+            transactionAnimation = mActivity;
+        }
+
+        // Set the custom transaction animation.
+        mFrgTransaction.setCustomAnimations(transactionAnimation.getAnimationEnter(), transactionAnimation.getAnimationExit(),
+                transactionAnimation.getAnimationPopEnter(),
+                transactionAnimation.getAnimationPopExit());
+
+        return this;
+    }
+
+}
