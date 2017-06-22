82ed727b35e29e874d7f38c33ddf095db78ebcd2
Jorge Costa <j.costa90@gmail.com>
2017-06-22 13:32:43 +0100

    fixed PMD and lint warnings

--- sample/src/main/java/com/massivedisaster/example/feature/sharedelements/FragmentSharedElementsOptions.java
+++ sample/src/main/java/com/massivedisaster/example/feature/sharedelements/FragmentSharedElementsOptions.java
@@ -27,111 +27,87 @@ package com.massivedisaster.example.feature.sharedelements;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewCompat;
-import android.view.LayoutInflater;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
 
 import com.massivedisaster.activitymanager.ActivityFragmentManager;
-import com.massivedisaster.activitymanager.activity.AbstractFragmentActivity;
+import com.massivedisaster.adal.adapter.OnChildClickListener;
+import com.massivedisaster.adal.fragment.AbstractBaseFragment;
 import com.massivedisaster.example.activity.ActivityPrimaryTheme;
 import com.massivedisaster.example.activitymanager.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Fragment Shared Elements Options
  */
-public class FragmentSharedElementsOptions extends Fragment implements View.OnClickListener {
+public class FragmentSharedElementsOptions extends AbstractBaseFragment {
 
-    private Button mBtnAddFragment, mBtnOpenFragment, mBtnReplaceFragment;
-    private ImageView mImgSharedElement;
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_shared_elements_options, container, false);
-
-        mBtnAddFragment = (Button) v.findViewById(R.id.btnAddFragment);
-        mBtnOpenFragment = (Button) v.findViewById(R.id.btnOpenFragment);
-        mBtnReplaceFragment = (Button) v.findViewById(R.id.btnReplaceFragment);
-
-        mImgSharedElement = (ImageView) v.findViewById(R.id.imgSharedElement);
-
-        return v;
+    protected int layoutToInflate() {
+        return R.layout.fragment_shared_elements_options;
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        mBtnAddFragment.setOnClickListener(this);
-        mBtnReplaceFragment.setOnClickListener(this);
-        mBtnOpenFragment.setOnClickListener(this);
-
-        FragmentTransaction f = getFragmentManager().beginTransaction();
-        f.commit();
+    protected void getFromBundle(Bundle bundle) {
+        //TODO
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnAddFragment:
-                add();
-                break;
-            case R.id.btnReplaceFragment:
-                replace();
-                break;
-            case R.id.btnOpenFragment:
-                open();
-                break;
-            default:
-                break;
-        }
+    protected void restoreInstanceState(@Nullable Bundle savedInstanceState) {
+        //TODO
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-    }
+    protected void doOnCreated() {
+        ImageAdapter adapter = new ImageAdapter(makeSequence(1, 10));
+        adapter.setOnChildClickListener(new OnChildClickListener<Integer>() {
+            @Override
+            public void onChildClick(View view, Integer integer, int position) {
+                openItem(view, integer);
+            }
+        });
 
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
+        RecyclerView rclItems = findViewById(R.id.rclItems);
+        rclItems.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        rclItems.setAdapter(adapter);
     }
 
     /**
-     * Add a new fragment, the transaction dont work here.
+     * Open a new Activity and share the element
+     *
+     * @param view    item view
+     * @param integer element id
      */
-    private void add() {
-        ActivityFragmentManager.add((AbstractFragmentActivity) getActivity(), FragmentSharedElement.class)
-                .addSharedElement(mImgSharedElement, ViewCompat.getTransitionName(mImgSharedElement))
-                .commit();
-    }
+    protected void openItem(View view, Integer integer) {
+        Bundle bundle = new Bundle();
+        bundle.putString("URL", "http://lorempixel.com/400/200/nature/" + integer);
 
-    /**
-     * Replace the fragment
-     */
-    private void replace() {
-        ActivityFragmentManager.replace((AbstractFragmentActivity) getActivity(), FragmentSharedElement.class)
-                .addSharedElement(mImgSharedElement, ViewCompat.getTransitionName(mImgSharedElement))
-                .commit();
-    }
-
-    /**
-     * Open a new fragment on a new activity
-     */
-    private void open() {
         ActivityFragmentManager.open(getActivity(), ActivityPrimaryTheme.class, FragmentSharedElement.class)
-                .addSharedElement(getActivity().findViewById(R.id.imgSharedElement),
-                        ViewCompat.getTransitionName(getActivity().findViewById(R.id.imgSharedElement)))
+                .setBundle(bundle)
+                .addSharedElement(view.findViewById(R.id.imgExample), "sharedElement")
                 .commit();
     }
+
+    /**
+     * Generate a List of integers between 2 values
+     *
+     * @param begin the lowest value
+     * @param end   the biggest value
+     * @return the List of items.
+     */
+    private List<Integer> makeSequence(int begin, int end) {
+        List<Integer> ret = new ArrayList<>();
+
+        for (int i = begin; i <= end; i++) {
+            ret.add(i);
+        }
+
+        return ret;
+    }
+
 }
