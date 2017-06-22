f18e64793dabac9e4f68c602c7dd40386288a10c
Jorge Costa <j.costa90@gmail.com>
2017-06-22 11:42:53 +0100

    Removed unused function

--- activity-fragment-manager/src/main/java/com/massivedisaster/activitymanager/activity/AbstractFragmentActivity.java
+++ activity-fragment-manager/src/main/java/com/massivedisaster/activitymanager/activity/AbstractFragmentActivity.java
@@ -80,8 +80,6 @@ public abstract class AbstractFragmentActivity extends AppCompatActivity impleme
         } else if (getDefaultFragment() != null) {
             performInitialTransaction(getFragment(getDefaultFragment().getCanonicalName()), null);
         }
-
-        ActivityFragmentManager.add(null, null);
     }
 
     /**
