783a2c5458de2d0c79e8b0c85609d8943553593d
Jorge Costa <j.costa90@gmail.com>
2017-06-22 14:21:13 +0100

    Added Shared elements title

--- sample/src/main/java/com/massivedisaster/example/feature/sharedelements/FragmentSharedElement.java
+++ sample/src/main/java/com/massivedisaster/example/feature/sharedelements/FragmentSharedElement.java
@@ -34,6 +34,8 @@ public class FragmentSharedElement extends AbstractBaseFragment {
 
     @Override
     protected void doOnCreated() {
+        getActivity().setTitle(R.string.image_detail);
+
         ImageView imgExample = findViewById(R.id.imgSharedElement);
 
         Glide.with(getContext())
